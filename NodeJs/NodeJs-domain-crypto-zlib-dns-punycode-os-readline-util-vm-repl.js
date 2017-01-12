d17769864d81da9a07910da7345bacd0def644db
luolei <18530076746@163.com>
2017-01-12 17:30:52 +0800

    add othersmodules 2

--- NodeJs/NodeJs-domain-crypto-zlib-dns-punycode-os-readline-util-vm-repl.js
+++ NodeJs/NodeJs-domain-crypto-zlib-dns-punycode-os-readline-util-vm-repl.js
@@ -410,5 +410,79 @@
           })
         })
   5、使用util模块提供的方法:
-
-
+    1、util.format(format,[....]);将第一个参数作为格式化字符串,将其他参数作为格式化该字符串时的选项,
+      util.format("呵呵%d哈哈%s哼哼%j",10,"hehe",{hello:"world"});
+    2、util.debug(string);同步方法,阻塞当前线程,将参数作为标准错误输出流输出,
+    3、util.error([...]);作用和debug方法的作用类似
+    4、util.puts([...]);同步方法,阻塞当前线程,将参数作为标准输出流输出,会产生一个新行
+    5、util.print([...]);同步方法,阻塞当前线程,将参数作为标准输出流输出,不会产生一个新行
+    6、util.log(string);将一个字符串作为标准输出流输出,字符串前输出当前系统时间,
+    7、util.inspect(object,[options]);返回一个字符串包含了该对象的信息,
+      1、options;Object,
+        1、showHidden;Boolean;default:false;为true时该对象信息包含该对象的不可枚举属性接属性值,
+        2、depth;Number,default:2;指定被查看的对象信息的深度,
+        3、colors;Boolean;default:false;为true时输出该对象信息时将该对象的各种属性值应用颜色
+        4、customInspect;Boolean;default:true;为true时查看对象信息时将调用对于被查看信息的对象自定义的inspect方法
+    8、util.inspect.styles对象定义对象属性值的样式
+      1、number;default:yellow;定义数值类型属性值的颜色,
+      2、boolean;default:yellow,定义布尔类型属性值的颜色,
+      3、string;default:green;定义字符串类型属性值的颜色,
+      4、date;default:magenta;定义日期类型属性值的颜色,
+      5、regexp;default:red;定义正则表达式的颜色,
+      6、null;default:bold;定义null值的字体,
+      7、undefined;default:grey;定义undefined值的颜色,
+      8、special;default:cyan;定义函数的颜色,
+    9、util.inspect.colors对象:定义对象属性值的样式
+      1、bold,italic,underline,white,grey,black,green,red,yellow
+      2、cyan:青色,magenta,紫红色,
+    10、util.isArray(object);Boolean,判断该参数是否为一个数组,
+    11、util.isRegExp(object);Boolean,判断该参数是否为一个正则表达式
+    12、util.isDate(object);Boolean,判断一个参数值是否为一个日期类型,
+    13、util.isError(object);Boolean,判断一个参数值是否为一个错误对象,
+    14、util.inherits(constructor,superConstructor);
+      1、constructor;子类构造函数,
+      2、superConstructor;父类构造函数,
+  6、使用vm模块改变脚本运行环境:
+    1、vm.runInThisContext(code,[filename]);作用和eval函数类似,在该上下文中不能访问任何模块中定义的本地变量,方法,对象,
+      1、code;String,指定需要运行的代码,
+      2、filename;记录代码运行时的堆栈信息,可以为空,
+    2、vm.runInNewContext(code,[sandbox],[filename]);作用和1、类似,但是不能访问NodeJs中定义的全局变量、属性、方法
+      1、code;String,指定需要运行的代码,
+      2、sandbox;Object,指定独立的上下文环境,
+      3、filename;记录代码运行时的堆栈信息,可以为空,
+    3、vm.createContext([initSandbox]);根据初始化的上下文对象创建另一个上下文对象,
+    4、vm.runInContext(code,[sandbox],[filename]);在该上下文环境中运行js代码,
+      var vm = require("vm");
+      var obj = {name:"Hello World"};
+      var context1 = vm.createContext(obj);
+      vm.runInContext("name = 'HeHe'",context1);
+      console.log(context1.name);
+      var context2 = vm.createContext(obj);
+      vm.runInContext("name = 'HeiHei'",context2);
+      console.log(context2.name);
+    5、var script = vm.createScript(code,[filename]);创建一个script对象并保存一段编译后的js代码,
+      1、code;String,指定需要编译的代码,
+      2、filename;记录代码运行时的堆栈信息,可以为空
+    5.1 script.runInThisContext();作用和vm.runInThisContext方法类似
+      var vm = require("vm");
+      var script = vm.createScript("globalVar += 1");
+      for(var i=1;i<100;i++){
+        script.runInThisContext();
+        console.log(globalVar);
+      }
+    5.2 script.runInNewContext([sandbox]);作用和vm.runInNewContext方法类似
+  7、自定义repl运行环境:
+    1、repl.start(options);运行并返回一个repl的实例对象,
+      1、options;Object,
+        1、prompt;default:">";修改repl运行环境的命令提示符,
+        2、input;default:"process.stin",指定需要用来读取流数据的对象,
+        3、output;default:"process.stdout",指定需要用来写入输出流数据的对象,
+        4、terminal;Boolean,为true时如果需要实时的将输入数据流进行输出,且需要在输出数据中写入ANST/VT100控制字符时,
+        5、write;Function,指定在输出表达式运行结果时用于格式化运行结果以及对运行结果应用各种颜色的函数
+        6、useColors;Boolean,指定在使用默认的writer属性时,输出表达式的结果是否使用颜色,
+          如果默认的writer属性值被修改,则该属性值失效,
+        7、useGlobal;Boolean,default:false;为true时当前上下文中的代码可以访问NodeJs中的全局变量、方法、对象
+        8、eval;Function,指定对输入表达式的执行方法,
+        9、ignoreUndefined;default:false;如果表达式的执行结果为undefined,则REPL环境不再显示该结果,
+        var repl = require("repl");
+        repl.start({useGlobal:false,ignoreUndefined:true});
