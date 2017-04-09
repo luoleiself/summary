0eea95e26fa71c267f7cb48bbb9305ee0db35517
luolei <18530076746@163.com>
2017-04-09 20:47:02 +0800

    modify php mysqlExtends

--- PHP/mysqlExtends.php
+++ PHP/mysqlExtends.php
@@ -6,8 +6,11 @@
       mysql_query("set names 'utf8'"); // 设置编码格式
       $res = mysql_query('select * from user limit 1',$link); // 执行查询语句
       $row = mysql_fetch_assoc($res); // 获取查询结果集中的数据
-      mysql_query('insert into user(name,age,class) value("王五","27","大一班")');
+      $result = mysql_query('insert into user(name,age,class) value("王五","27","大一班")');
+      mysql_error($link); // 获取执行语句失败原因
       echo mysql_insert_id(); // 获取插入记录成功的id
+      mysql_close($link); // 关闭数据库链接
+      $bool = mysql_select_db("test"); // 选择数据库,选择成功返回true,否则返回false
     2. mysqli: $link = mysqli_connect("mysql_host","mysql_user","mysql_password");
     2. PDO:(php data object),PHP访问数据库定义了一个轻量级的一致接口
       1. $dsn = "mysql:dbname = testdb;host = 127.0.0.1";
