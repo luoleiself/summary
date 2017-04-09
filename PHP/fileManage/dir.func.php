35e467d51284bf1fa65665ccf055bab6f881caca
luolei <18530076746@163.com>
2017-04-09 11:28:09 +0800

    modify php/dir.func.php

--- PHP/fileManage/dir.func.php
+++ PHP/fileManage/dir.func.php
@@ -79,4 +79,80 @@
     closedir($handle);
     return "复制成功";
   }
+  /**
+   * [renameFolder 重命名文件夹]
+   * @param  [string]                   $oldname [旧文件夹名称]
+   * @param  [string]                   $newname [新文件夹名称]
+   * @return [type]                            [description]
+   * @author luolei
+   * @date   2017-04-09T10:53:57+0800
+   */
+  function renameFolder($oldname,$newname){
+    if(checkFilename(basename($newname))){
+      if(!file_exists($newname)){
+        if(rename($oldname, $newname)){
+          $mes = "重命名成功";
+        }else{
+          $mes = "重命名失败";
+        }
+      }else{
+        $mes = "存在同名文件";
+      } 
+    }else{
+      $mes = "非法文件夹名称";
+    }
+    return $mes;
+  }
+  /**
+   * [cutFolder 剪切文件夹]
+   * @param  [string]                   $src [源文件夹名称]
+   * @param  [string]                   $dst [目标文件夹名称]
+   * @return [type]                        [description]
+   * @author luolei
+   * @date   2017-04-09T11:09:05+0800
+   */
+  function cutFolder($src,$dst){
+    if(file_exists($dst)){
+      if(is_dir($dst)){
+        if(!file_exists($dst."/".basename($src))){
+          if(rename($src,$dst."/".basename($src))){
+            $mes = "剪切成功";
+          }else{
+            $mes = "剪切失败";
+          }
+        }else{
+          $mes = "存在同名文件夹";
+        }
+      }else{
+        $mes = "不是一个文件夹";
+      }
+    }else{
+      $mes = "目标文件夹不存在";
+    }
+    return $mes;
+  }
+  /**
+   * [delFolder 删除文件夹]
+   * @param  [string]                   $path [源文件夹路径]
+   * @return [type]                         [description]
+   * @author luolei
+   * @date   2017-04-09T11:24:45+0800
+   */
+  function delFolder($path){
+    $handle = opendir($path);
+    while (($item = readdir($handle)) !== false) {
+      if($item != "." && $item != ".."){
+        if(is_file($path."/".$item)){
+          unlink($path."/".$item);
+        }
+        if(is_dir($path."/".$item)){
+          $func = __FUNCTION__;
+          $func($path."/".$item);
+        }
+      }
+    }
+    closedir($handle);
+    rmdir($path);
+    return "文件夹删除成功";
+  }
  ?>
