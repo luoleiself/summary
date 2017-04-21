fcb0a911152c6076205865a09c341ab2f6ad1ba3
luolei <18530076746@163.com>
2017-04-21 18:52:18 +0800

    modify nodejs server.js

--- NodeJs/express/server.js
+++ NodeJs/express/server.js
@@ -15,7 +15,7 @@ app.get("/index.html/:id?/:name?", function(req, res) {
 }).listen("9000", "localhost");
 // http.createServer(app).listen("9000");
 //*/
-var app = require("express")();
+/*var app = require("express")();
 var http = require("http");
 app.get("/index.html/:id(\\d+)", function(req, res, next) {
   if (req.params.id > 10) {
@@ -27,4 +27,18 @@ app.get("/index.html/:id(\\d+)", function(req, res, next) {
 app.get("/index.html/:id(\\d+)", function(req, res) {
   res.send("Hello World");
 });
+app.listen("9000", "localhost");*/
+var app = require("express")();
+var http = require("http");
+app.get("/index", function(req, res) {
+  res.writeHeader("200", { "content-type": "text/html" });
+  res.write("<head><meta charset='utf-8'></head>");
+  res.end("<h1 style='background:#e4393c;color:blue;'>请求路径为/index</h1>");
+});
+app.get("/about", function(req, res) {
+  res.writeHeader("200", { "content-type": "text/html" });
+  res.write("<head><meta charset='utf-8'></head>");
+  res.end("<h1 style='background:#e4393c;color:blue;'>请求路径为/about</h1>");
+});
+
 app.listen("9000", "localhost");
