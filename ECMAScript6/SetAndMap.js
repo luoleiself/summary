d3f608ab8a432c20877088a42c798a5805d20c48
luolei <18530076746@163.com>
2017-06-05 21:47:56 +0800

    Update SetAndMap.js

--- ECMAScript6/SetAndMap.js
+++ ECMAScript6/SetAndMap.js
@@ -86,6 +86,228 @@
           let set = new Set([1, 2, 3, 4, 5]);
           set = new Set([...set].filter(x => (x % 2) == 0));
           // 返回Set结构：{2, 4}
-2.WeakSet:
-3.Map:
-4.WeakMap:
+2.WeakSet:与Set结构类似;
+  1.区别:
+    1.WeakSet的成员只能是对象,而不能是其他类型的值
+    2.不计入垃圾回收机制
+  2.构造函数:
+    eg:const a = [[1, 2], [3, 4]];
+      const ws = new WeakSet(a);
+      // WeakSet {[1, 2], [3, 4]} ,数组a的成员是对象,成为weakSet的成员
+      const b = [3, 4];
+      const ws = new WeakSet(b); // b的成员不是对象,加入weakSet报错
+      // Uncaught TypeError: Invalid value used in weak set(…)
+  3.方法:
+    1.WeakSet.prototype.add(value);向WeakSet实例添加一个新成员
+    2.WeakSet.prototype.delete(value);清除WeakSet实例的指定成员
+    3.WeakSet.prototype.has(value);判断WeakSet实例是否包含指定成员
+      eg:const ws = new WeakSet();
+        const obj = {};
+        const foo = {};
+        ws.add(window);
+        ws.add(obj);
+        ws.has(window); // true
+        ws.has(foo);    // false
+        ws.delete(window);
+        ws.has(window);    // false 
+  4.不能遍历:因为成员都是弱引用,随时可能消失,遍历机制无法保证成员的存在 
+    eg:const foos = new WeakSet()
+        class Foo {
+          constructor() {
+            foos.add(this)
+          }
+          method () {
+            if (!foos.has(this)) {
+              throw new TypeError('Foo.prototype.method 只能在Foo的实例上调用！');
+            }
+          }
+        }
+3.Map:Object只支持使用字符串作为键名,Map提供了'值-值'的对应关系
+  1.构造函数:
+    eg:const m = new Map();
+      const o = {p: 'Hello World'};
+      m.set(o, 'content')
+      m.get(o) // "content"
+      m.has(o) // true
+      m.delete(o) // true
+      m.has(o) // false
+      // 参数接收一个二维数组,该数组的成员是一个个表示键值对的数组
+      const map = new Map([
+        ['name', '张三'],
+        ['title', 'Author']
+      ]);
+      map.size // 2
+      map.has('name') // true
+      map.get('name') // "张三"
+      map.has('title') // true
+      map.get('title') // "Author"、
+      // set和map可以生成新的map
+      const set = new Set([
+        ['foo', 1],
+        ['bar', 2]
+      ]);
+      const m1 = new Map(set);
+      m1.get('foo') // 1
+      const m2 = new Map([['baz', 3]]);
+      const m3 = new Map(m2);
+      m3.get('baz') // 3
+  2.属性和操作方法:
+    Map.prototype.size;返回Map结构的成员总数;
+      eg:const map = new Map();
+        map.set('foo', true);
+        map.set('bar', false);
+        map.size // 2
+    Map.prototype.set(key,value);设置键名并返回Map结构
+      eg:const m = new Map();
+        m.set('edition', 6)        // 键是字符串
+        m.set(262, 'standard')     // 键是数值
+        m.set(undefined, 'nah')    // 键是 undefined
+    Map.prototype.get(key);获取指定key对应的value,未找到返回undefined
+      eg:const m = new Map();
+        const hello = function() {console.log('hello');};
+        m.set(hello, 'Hello ES6!') // 键是函数
+        m.get(hello)  // Hello ES6!
+    Map.prototype.has(key);判断Map结构是否包含指定key;返回boolean
+      eg:const m = new Map();
+        m.set('edition', 6);
+        m.set(262, 'standard');
+        m.set(undefined, 'nah');
+        m.has('edition')     // true
+        m.has('years')       // false
+        m.has(262)           // true
+        m.has(undefined)     // true
+    Map.prototype.delete(key);删除指定key,返回boolean
+      eg:const m = new Map();
+        m.set(undefined, 'nah');
+        m.has(undefined)     // true
+        m.delete(undefined)
+        m.has(undefined)       // false
+    Map.prototype.clear();清除所有成员;
+      eg:let map = new Map();
+        map.set('foo', true);
+        map.set('bar', false);
+        map.size // 2
+        map.clear()
+        map.size // 0
+  3.遍历方法:
+    keys();返回键名的遍历器;
+    values();返回键值的遍历器;
+    entries();返回所有成员的遍历器;
+    forEach();遍历Map结构的所有成员;
+    Map结构转为数组结构:使用扩展运算符...
+      eg:const map = new Map([
+          [1, 'one'],
+          [2, 'two'],
+          [3, 'three'],
+        ]);
+        [...map.keys()]
+        // [1, 2, 3]
+        [...map.values()]
+        // ['one', 'two', 'three']
+        [...map.entries()]
+        // [[1,'one'], [2, 'two'], [3, 'three']]
+        [...map]
+        // [[1,'one'], [2, 'two'], [3, 'three']]      
+  4.与其他数据结构的互相转换:
+    1.Map结构转为数组:使用扩展运算符...
+      eg:const myMap = new Map()
+          .set(true, 7)
+          .set({foo: 3}, ['abc']);
+        [...myMap]
+        // [ [ true, 7 ], [ { foo: 3 }, [ 'abc' ] ] ]
+    2.数组转为Map:使用Map构造函数
+      eg:new Map([
+          [true, 7],
+          [{foo: 3}, ['abc']]
+        ])
+        // Map {
+        //   true => 7,
+        //   Object {foo: 3} => ['abc']
+        // }
+    3.Map结构转为对象:如果Map的键都是字符串可以转为对象
+      eg:function strMapToObj(strMap) {
+          let obj = Object.create(null);
+          for (let [k,v] of strMap) {
+            obj[k] = v;
+          }
+          return obj;
+        }
+        const myMap = new Map()
+          .set('yes', true)
+          .set('no', false);
+        strMapToObj(myMap)
+        // { yes: true, no: false }
+    4.对象转为Map结构:
+      eg:function objToStrMap(obj) {
+          let strMap = new Map();
+          for (let k of Object.keys(obj)) {
+            strMap.set(k, obj[k]);
+          }
+          return strMap;
+        }
+        objToStrMap({yes: true, no: false})
+        // Map {"yes" => true, "no" => false}
+    5.Map结构转为JSON:
+      1.Object(JSON):Map结构的键名都是字符串
+        eg:function strMapToJson(strMap) {
+            return JSON.stringify(strMapToObj(strMap));
+          }
+          let myMap = new Map().set('yes', true).set('no', false);
+          strMapToJson(myMap)
+          // '{"yes":true,"no":false}'
+      2.Array(JSON):Map结构的键名有非字符串
+        eg:function mapToArrayJson(map) {
+            return JSON.stringify([...map]);
+          }
+          let myMap = new Map().set(true, 7).set({foo: 3}, ['abc']);
+          mapToArrayJson(myMap)
+          // '[[true,7],[{"foo":3},["abc"]]]'
+    6.JSON转为Map:
+      1.正常情况:所有键名都是字符串
+        eg:function jsonToStrMap(jsonStr) {
+            return objToStrMap(JSON.parse(jsonStr));
+          }
+          jsonToStrMap('{"yes": true, "no": false}')
+          // Map {'yes' => true, 'no' => false}
+      2.特殊情况:JSON为数组,每个成员又是一个有两个成员的数组;
+        eg:function jsonToMap(jsonStr) {
+            return new Map(JSON.parse(jsonStr));
+          }
+          jsonToMap('[[true,7],[{"foo":3},["abc"]]]')
+          // Map {true => 7, Object {foo: 3} => ['abc']}
+4.WeakMap:与Map结构类似;
+  1.区别:
+    1.WeakMap只接受对象作为键名(null除外),不接受其他类型的值作为键名
+    2.WeakMap的键名所指向的对象,不计入垃圾回收机制
+  1.构造函数:
+    eg:// WeakMap 可以使用 set 方法添加成员
+      const wm1 = new WeakMap();
+      const key = {foo: 1};
+      wm1.set(key, 2);
+      wm1.get(key) // 2
+      // WeakMap 也可以接受一个数组，
+      // 作为构造函数的参数
+      const k1 = [1, 2, 3];
+      const k2 = [4, 5, 6];
+      const wm2 = new WeakMap([[k1, 'foo'], [k2, 'bar']]);
+      wm2.get(k2) // "bar"
+      const map = new WeakMap();
+      map.set(1, 2)
+      // TypeError: 1 is not an object!
+      map.set(Symbol(), 2)
+      // TypeError: Invalid value used as weak map key
+      map.set(null, 2)
+      // TypeError: Invalid value used as weak map key
+  2.方法:
+    Map get(key);获取指定key对应的value
+    Map set(key,value);添加某个值
+    boolean has(key);判断Map结构是否包含指定key
+    boolean delete(key);删除指定key
+  3.用途:Dom节点作为键名,绑定监听事件更新状态,Dom节点删除,该状态自动消失
+    eg:let myElement = document.getElementById('logo');
+      let myWeakmap = new WeakMap();
+      myWeakmap.set(myElement, {timesClicked: 0});
+      myElement.addEventListener('click', function() {
+        let logoData = myWeakmap.get(myElement);
+        logoData.timesClicked++;
+      }, false);
