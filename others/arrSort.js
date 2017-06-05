eabc0525f04632bdcac17bbe4c7fade9aa4d1d16
luolei <18530076746@163.com>
2017-06-05 10:56:04 +0800

    Update arrSort.js

--- others/arrSort.js
+++ others/arrSort.js
@@ -183,29 +183,3 @@ for (var fraction = Math.floor(len / 2); fraction > 0; fraction = Math.floor(fra
   }
 }
 console.log(arr);
-6、 递归遍历实现树的数据格式
-var data = [
-  { id: 1, title: 'pid=0', pid: 0 },
-  { id: 2, title: 'pid=1', pid: 1 },
-  { id: 3, title: 'pid=2', pid: 2 },
-  { id: 4, title: 'pid=2', pid: 2 },
-  { id: 5, title: 'pid=1', pid: 1 },
-  { id: 6, title: 'pid=5', pid: 5 },
-  { id: 7, title: 'pid=0', pid: 0 }
-];
-
-function treeRecursion(arr, parentId) {
-  var result = [],
-    temp;
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i].pid == parentId) {
-      result.push(arr[i]);
-      temp = treeRecursion(arr, arr[i].id);
-      if (temp.length > 0) {
-        result[i].children = temp;
-      }
-    }
-  }
-  return result;
-}
-console.log(treeRecursion(data, 0));
