fa9f4ecaf297cc03599d4c666f53e4836db2e12d
luolei <18530076746@163.com>
2017-06-07 11:45:45 +0800

    modify others/window.js

--- others/window.js
+++ others/window.js
@@ -1,3 +1,4 @@
+// 修改历史记录阻止后退功能
 $(document).ready(function() {
   if (window.history && window.history.pushState) {
     $(window).on('popstate', function() {
@@ -8,3 +9,31 @@ $(document).ready(function() {
   window.history.pushState('forward', null, ''); //在IE中必须得有这两行
   window.history.forward(1);
 });
+
+// js调用本地摄像头拍照
+var video = document.querySelector("video");
+var canvas = document.querySelector('canvas');
+// 调用媒体设备
+navigator.mediaDevices.getUserMedia({
+  // audio: true,
+  video: true
+}, function(mediaStream) { // 成功时回调函数
+  video.src = window.URL.createObjectURL(mediaStream);
+  video.play();
+}, function(error) { // 失败时回调函数
+  alert('video error: ' + err);
+});
+var context = canvas.getContext('2d');
+//捕获并保存帧内容
+function captureAndSaveFrame() {
+  context.drawImage(video, 0, 0, 300, 300);
+  console.log(canvas.toDataURL()); //转为base64并保存
+}
+//定时捕获
+function timer(delta) {
+  setTimeout(function() {
+    captureAndSaveFrame();
+    timer(delta)
+  }, delta || 500);
+}
+timer();
