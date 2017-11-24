3e7c0891a161a6e4a4f112cfadc0ce808b43aba1
luolei <18530076746@163.com>
2017-11-24 17:19:36 +0800

    移动表格实例

--- others/uploadImg.js
+++ others/uploadImg.js
@@ -14,33 +14,62 @@ function getFileUrl(sourceId) {
 	return url;
 }
 /**
- * @param {any} url 
+ * @param {any} sourceId 
+ * @param {any} imgFormat 
  * @param {any} callback 
- * @param {any} outputFormat 
  */
-function convertImgToBase64(url, callback, outputFormat) {
+function convertImgToBase64(sourceId, imgFormat, callback) {
 	var canvas = document.createElement('CANVAS'),
 		ctx = canvas.getContext('2d'),
-		img = new Image;
+		img = new Image();
 	img.crossOrigin = 'Anonymous';
+	img.src = getFileUrl(sourceId);
 	img.onload = function () {
 		canvas.height = img.height;
 		canvas.width = img.width;
 		ctx.drawImage(img, 0, 0);
-		var dataURL = canvas.toDataURL(outputFormat || 'image/png');
+		var dataURL = canvas.toDataURL(imgFormat || 'image/png');
 		callback.call(this, dataURL);
 		canvas = null;
 	};
-	img.src = url;
 }
 /**
  * 
  */
 document.querySelector("#submit").onclick = function () {
-	var fsrc;
-	fsrc = getFileUrl("myfile");
-	convertImgToBase64(fsrc, function (base64Img) {
+	convertImgToBase64("myfile", '', function (base64Img) {
 		console.log(base64Img);
 		// Base64DataURL
 	});
 };
+
+// 示例:
+(function () {
+	function convertImgToBase64(sourceId, imgFormat, callback) {
+		var canvas = document.createElement('CANVAS'),
+			ctx = canvas.getContext('2d'),
+			img = new Image();
+		img.crossOrigin = 'Anonymous';
+		img.src = getFileUrl(sourceId);
+		img.onload = function () {
+			canvas.height = img.height;
+			canvas.width = img.width;
+			ctx.drawImage(img, 0, 0);
+			var dataURL = canvas.toDataURL(imgFormat || 'image/png');
+			callback.call(this, dataURL);
+			canvas = null;
+		};
+	}
+
+	function getFileUrl(sourceId) {
+		var url;
+		if (navigator.userAgent.indexOf("MSIE") >= 1) { // IE
+			url = document.getElementById(sourceId).value;
+		} else if (navigator.userAgent.indexOf("Firefox") > 0) { // Firefox
+			url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));
+		} else if (navigator.userAgent.indexOf("Chrome") > 0) { // Chrome
+			url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));
+		}
+		return url;
+	}
+})()
