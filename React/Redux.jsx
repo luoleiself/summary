9aa4afac8acd26c50bb06620400a3c4e544e4ecb
luoleiself <luoleiself@163.com>
2021-02-08 10:56:41 +0800

    redux princple

--- React/Redux.jsx
+++ React/Redux.jsx
@@ -4,7 +4,12 @@ import ReduxThunk from 'redux-thunk'; // 改造 store.dispatch() 支持函数作
 import ReduxPromise from 'redux-promise'; // 改造 store.dispatch() 支持 Promise 对象作为参数
 
 import { createAction, createActions, handleAction, handleActions, combineActions } from 'redux-actions';
-
+/**
+ * Redux的三大原则
+ *  1. 单一数据源, 整个应用的 state 被储存在一棵object tree 中.并且这个 object tree 只存在于唯一一个 store 中.
+ *  2. State 是只读的, 唯一改变 state 的方法就是触发 action. action 是一个用于描述已发生事件的普通对象.
+ *  3. 使用纯函数来执行修改, 描述 action 如何改变 state tree.
+ */
 /**
  * Store 的职责
  *  1. 维持应用的 state
