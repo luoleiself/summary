f7ef4bf2c21489c4e13817239a98475dba06bce1
luoleiself <luoleiself@163.com>
2021-04-14 11:56:43 +0800

    Vuex 4.x

--- Vue/4.x.Vuex.js
+++ Vue/4.x.Vuex.js
@@ -1,6 +1,49 @@
-import { createApp } from 'vue';
-import { createStore } from 'vuex';
+import { createApp, computed, watchEffect } from 'vue';
+import { createStore, useStore, mapState, mapGetters, mapMutations, mapActions, createNamespacedHelpers } from 'vuex';
 // initialization
-const store = createStore({});
+const store = createStore({
+  state() {
+    return { count: 0 };
+  },
+  getters: {},
+  mutations: {
+    ['COUNT_ADD'](state, payload) {
+      state.count = payload;
+    },
+  },
+  actions: {},
+});
 const app = createApp({});
 app.use(store);
+
+// Composition API
+export default {
+  setup() {
+    const store = useStore();
+
+    const count = computed({
+      set: (val) => {
+        console.log('computed hooks set count by store... ', val);
+        store.commit('COUNT_ADD', val);
+      },
+      get: () => {
+        let count = store.state.count;
+        console.log('computed hooks get count... ', count);
+        return count;
+      },
+    });
+
+    watchEffect(
+      () => {
+        console.log('watchEffect... ', count.value);
+      },
+      { flush: 'post' }
+    );
+
+    const increment = () => {
+      count.value += 1;
+    };
+    
+    return { count, increment };
+  },
+};
