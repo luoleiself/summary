6787c7a54b8cccb0e228c9788f948f30927f179d
luoleiself <luoleiself@163.com>
2021-08-18 14:50:07 +0800

    vue 3.x slot and emit

--- others/utils.js
+++ others/utils.js
@@ -31,3 +31,43 @@ export const throttle = function (fn, delay) {
     }
   };
 };
+/**
+ * @description 随机生成颜色
+ * @return {String}
+ */
+export const randomColor = function () {
+  return '#' + ('00000' + ((Math.random() * 0x1000000) << 0).toString(16)).slice(-6);
+};
+
+/**
+ * @description 获取浏览器类型和版本
+ * @return {String}
+ */
+export const getExplore = function () {
+  var sys = {},
+    ua = navigator.userAgent.toLowerCase(),
+    s;
+  (s = ua.match(/rv:([\d.]+)\) like gecko/))
+    ? (sys.ie = s[1])
+    : (s = ua.match(/msie ([\d\.]+)/))
+    ? (sys.ie = s[1])
+    : (s = ua.match(/edge\/([\d\.]+)/))
+    ? (sys.edge = s[1])
+    : (s = ua.match(/firefox\/([\d\.]+)/))
+    ? (sys.firefox = s[1])
+    : (s = ua.match(/(?:opera|opr).([\d\.]+)/))
+    ? (sys.opera = s[1])
+    : (s = ua.match(/chrome\/([\d\.]+)/))
+    ? (sys.chrome = s[1])
+    : (s = ua.match(/version\/([\d\.]+).*safari/))
+    ? (sys.safari = s[1])
+    : 0;
+  // 根据关系进行判断
+  if (sys.ie) return 'IE: ' + sys.ie;
+  if (sys.edge) return 'EDGE: ' + sys.edge;
+  if (sys.firefox) return 'Firefox: ' + sys.firefox;
+  if (sys.chrome) return 'Chrome: ' + sys.chrome;
+  if (sys.opera) return 'Opera: ' + sys.opera;
+  if (sys.safari) return 'Safari: ' + sys.safari;
+  return 'Unkonwn';
+};
