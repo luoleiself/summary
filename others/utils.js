cdd97c8c5cf836897ff8c7429d2ba12d3eb5451c
luoleiself <luoleiself@163.com>
2021-12-11 17:16:51 +0800

    utils arrayToTree

--- others/utils.js
+++ others/utils.js
@@ -112,3 +112,31 @@ export const copyText = (text) => {
     }
   }
 };
+
+/* 扁平数据结构转 tree */
+export const arrayToTree = function (arr) {
+  const result = []; // 存放结果集
+  const itemMap = {};
+  for (const item of arr) {
+    const id = item.id;
+    const pid = item.pid;
+
+    if (!itemMap[id]) {
+      itemMap[id] = { children: [] };
+    }
+
+    itemMap[id] = { ...item, children: itemMap[id]['children'] };
+
+    const treeItem = itemMap[id];
+
+    if (pid === 0) {
+      result.push(treeItem);
+    } else {
+      if (!itemMap[pid]) {
+        itemMap[pid] = { children: [] };
+      }
+      itemMap[pid].children.push(treeItem);
+    }
+  }
+  return result;
+};
